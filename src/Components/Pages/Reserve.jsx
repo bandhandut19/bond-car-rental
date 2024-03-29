c57d3c78819faa41717a4a67da22a4f31c7c4ac0
bandhandut19 <bondon21081@gmail.com>
2024-03-30 02:21:08 +0600

    initial charges summary table setup done

--- src/Components/Pages/Reserve.jsx
+++ src/Components/Pages/Reserve.jsx
@@ -7,6 +7,7 @@ import { CiCalendarDate } from "react-icons/ci";
 const Reserve = () => {
     const { setFirstName, setLastName, setEmail, setPhone,
         setCdw, setLi, setRt,
+        cdw, li, rt,
         setVehicleType,
         setVehicle,
 
@@ -133,7 +134,7 @@ const Reserve = () => {
                                 <h1 className="label-text">Duration</h1>
                             </div>
                             <div>
-                                <div className="border-2 py-1 px-2 rounded-md">
+                                <div className="border-2 py-1 px-2 rounded-md bg-green-100">
                                     <p className="text-sm">
                                         {duration && (duration.weeks > 0 || duration.days > 0 || duration.hours > 0 || duration.minutes > 0) ? (
                                             <span>
@@ -222,6 +223,65 @@ const Reserve = () => {
                 {/* Charges Summary */}
                 <div>
                     <h1 className="border-b-2 border-purple-500 font-bold text-xl mb-5">Charges Summary</h1>
+
+                    <div className="border-2 border-purple-500 px-5 py-3 bg-purple-100 rounded-md overflow-x-auto">
+                        <table className="table">
+                            <thead className=" border-b-2 border-purple-600">
+                                <tr  >
+
+                                    <th className="text-black">Charge</th>
+                                    <th className="text-black">Unit</th>
+                                    <th className="text-black">Rate</th>
+                                    <th className="text-black">Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* daily */}
+                                <tr>
+                                    <td>Daily</td>
+                                    <td>1</td>
+                                    <td>10</td>
+                                    <td>100</td>
+                                </tr>
+                                {/* weekly */}
+                                <tr>
+                                    <td>Weekly</td>
+                                    <td>1</td>
+                                    <td>10</td>
+                                    <td>100</td>
+                                </tr>
+
+                                {/* additonal charges */}
+                                {
+                                    (cdw && <tr>
+                                        <td>Collision Damage Wavier</td>
+                                        <td></td>
+                                        <td>$9.00</td>
+                                        <td>$9.00</td>
+                                    </tr>)
+                                }
+                                {
+                                    (li && <tr>
+                                        <td>Liability Insurance</td>
+                                        <td></td>
+                                        <td>$15.00</td>
+                                        <td>$15.00</td>
+                                    </tr>)
+                                }
+                                {
+                                    (rt && <tr>
+                                        <td>Collision Damage Wavier</td>
+                                        <td></td>
+                                        <td>11.5%</td>
+                                        <td>tax amount</td>
+                                    </tr>)
+                                }
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
 
                 </div>
 
