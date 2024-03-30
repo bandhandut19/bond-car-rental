55fe0f2bce3862558e9d970e16654ce32730833a
bandhandut19 <bondon21081@gmail.com>
2024-03-30 23:11:06 +0600

    landing page fixed

--- src/Components/Layouts/Car.jsx
+++ src/Components/Layouts/Car.jsx
@@ -3,26 +3,26 @@ const Car = ({ car }) => {
 
     return (
         <div>
-            <div className="card mt-5 gap-5 bg-green-100 shadow-xl items-center justify-center">
+            <div className="card mt-5 gap-5 bg-purple-200 shadow-xl items-center justify-center lg:p-5 py-10">
                 <div className=' p-5'>
                 <img style={{width: '250px', height:'150px'}} src={car.imageURL} alt="car_image" />
                 </div>
                 <div className="px-5">
-                    <h2 className="card-title">Model: <span className='text-green-500'>{car.model}</span></h2>
-                    <h2 className="card-title">Type: <span className='text-green-500'>{car.type}</span></h2>
+                    <h2 className="card-title">Model: <span className='text-blue-500'>{car.model}</span></h2>
+                    <h2 className="card-title">Type: <span className='text-blue-500'>{car.type}</span></h2>
                     <h2 className="card-title">
-                        Features: 
-                        <span className='text-green-500'>
-                            <div className='flex lg:flex-row flex-col gap-2'>
+                        Special Feature: 
+                        <span className='text-blue-500'>
+                            <div className='flex lg:flex-row flex-col gap-2 '>
 
-                            {car.features.slice(0,2).map((feature,index)=> <p key={index}>{feature},</p>)}
+                            {car.features.slice(0,1).map((feature,index)=> <p key={index}>{feature}</p>)}
                             </div>
                         </span></h2>
                     
 
-                    <div className="card-actions justify-center mt-10 mb-5">
+                    {/* <div className="card-actions justify-center mt-10 mb-5">
                         <button className="btn bg-green-400">View Details</button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
